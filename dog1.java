a79da872116176204fe7c44998262b3eefcf8398
Jayshree Kiran <93006422+jjaysh@users.noreply.github.com>
2021-12-05 22:51:20 +0530

    Add files via upload

---
+++ dog1.java
@@ -0,0 +1,23 @@
+public class dog1
+{
+    public void speak()
+    {
+        System.out.println("woof!");
+    }
+
+    public static void main(String[] args)
+    {
+        Dog d = new Dog();
+        d.speak();
+        Dog b = new Beagle();
+        b.speak();
+    }
+}
+
+class Beagle extends Dog
+{
+    public void speak()
+    {
+        System.out.println("arf arf");
+    }
+}
